ecfa2e361c0d84c883abe30810f163bdaba7edfc
Jean Carlors PAUL <35576539+jeanCarlors@users.noreply.github.com>
2024-12-11 22:17:15 -0500

    Update results_summary_maker.py

--- src/results_summary_maker.py
+++ src/results_summary_maker.py
@@ -1,24 +1,22 @@
 import json
 
-with open('antlr4_results.json', 'r') as file:
-    results = json.load(file)
+def results_summary_maker():
+    results_summary_path = "results_summary.json"
 
+    with open('antlr4_results.json', 'r') as file:
+        results = json.load(file)
 
-def results_summary_maker(results):
-    with open("results_summary.txt", "w") as file:
-        file.write("===================== Here are the results: =====================" + "\n\n")
     for item in results.keys():
         if isinstance(results[item], dict):
-            with open("results_summary.txt", "a") as file:
-                file.write("commit_hash: " + results[item]["commit_hash"] + "\n")
-                file.write("refactoring_id: " + results[item]["refactoring_id"] + "\n")
-                file.write("refactoring_type: " + results[item]["refactoring_type"] + "\n")
-                file.write("prompt_approach_item: " + results[item]["prompt_approach_item"] + "\n")
             for inner_item in results[item].keys():
-                if '_length' in inner_item:
-                    with open("results_summary.txt", "a") as file:
-                        file.write(inner_item + ": " + str(results[item][inner_item]) + "\n")
-            with open("results_summary.txt", "a") as file:
-                file.write("="*150 + "\n\n")
+                if isinstance(results[item][inner_item], list):
+                    results[item][inner_item] = len(results[item][inner_item])
+        elif isinstance(results[item], list):
+            results[item] = len(results[item])
 
-results_summary_maker(results)
+    json_object = json.dumps(results, indent=4)
+    with open(results_summary_path, 'w') as results_file:
+        results_file.write(json_object)
+
+
+results_summary_maker()
