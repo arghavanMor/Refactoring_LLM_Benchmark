77d773f6ff8e5839a75ca2d80731ea405aa0ff04
jeanCarlors <jeancarlorspaul@yahoo.com>
2025-02-19 00:45:31 -0500

    This commit allows you to add several refactorings at once.

--- src/main/java/org/refactoring/CodeModifier.java
+++ src/main/java/org/refactoring/CodeModifier.java
@@ -6,43 +6,56 @@ import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import java.io.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class CodeModifier {
 
-    public static Boolean codeModifier(String pathClass, String methodName, String newMethodCode, String classCode, String otherCode) throws FileNotFoundException {
+    public static Boolean codeModifier(String pathClass, String mainMethodName, String mainMethodCode, ArrayList<String> otherMethodsCode, ArrayList<String> classesCode, ArrayList<String> otherCode) throws FileNotFoundException {
         File projectDir = new File(pathClass);
 
+        /*
         System.out.println("----------------------------------------");
         System.out.println(pathClass);
         System.out.println("----------------------------------------");
-        System.out.println(methodName);
+        System.out.println(mainMethodName);
         System.out.println("----------------------------------------");
-        System.out.println(newMethodCode);
+        System.out.println(mainMethodCode);
         System.out.println("----------------------------------------");
-        System.out.println(classCode);
+        System.out.println(otherMethodsCode.toString());
         System.out.println("----------------------------------------");
-        System.out.println(otherCode);
+        System.out.println(classesCode.toString());
         System.out.println("----------------------------------------");
+        System.out.println(otherCode.toString());
+        System.out.println("----------------------------------------");
+        */
 
-        /*
         CompilationUnit compilationUnitNode = StaticJavaParser.parse(projectDir);
-        // Define a new method to replace the old one
-        MethodDeclaration newMethod = StaticJavaParser.parseMethodDeclaration(newMethodCode);
-        // Find and replace the original method
+        MethodDeclaration mainMethod = StaticJavaParser.parseMethodDeclaration(mainMethodCode);
         compilationUnitNode.findAll(ClassOrInterfaceDeclaration.class).forEach(eachClass -> {
-            eachClass.getMethodsByName(methodName).forEach(method -> {
-                System.out.printf("----------------- %s is refactoring -----------------", methodName);
-                method.replace(newMethod);
+            eachClass.getMethodsByName(mainMethodName).forEach(method -> {
+                System.out.println("----------------- Refactoring starting  -----------------");
+                method.replace(mainMethod);
 
-                if (!classCode.isEmpty()){
-                    BodyDeclaration<?> innerClass = StaticJavaParser.parseBodyDeclaration(classCode);
-                    if (innerClass instanceof ClassOrInterfaceDeclaration) {
-                      eachClass.addMember((ClassOrInterfaceDeclaration) innerClass);
-                        System.out.printf("----------------- Inner class added to %s -----------------", eachClass);
+                for (String otherMethodCode : otherMethodsCode) {
+                    if (!otherMethodCode.isEmpty()){
+                        MethodDeclaration otherMethod = StaticJavaParser.parseMethodDeclaration(otherMethodCode);
+                        {
+                            eachClass.addMember(otherMethod);
+                        }
                     }
                 }
+
+                for (String classCode : classesCode) {
+                    if (!classCode.isEmpty()){
+                        BodyDeclaration<?> innerClass = StaticJavaParser.parseBodyDeclaration(classCode);
+                        if (innerClass instanceof ClassOrInterfaceDeclaration) {
+                            eachClass.addMember(innerClass);
+                        }
+                    }
+                }
+                System.out.println("----------------- Refactoring process terminated -----------------");
             });
         });
 
@@ -51,23 +64,25 @@ public class CodeModifier {
          } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        */
-        return true;
 
+        return true;
     }
 
-
     public static void main(String[] args) throws FileNotFoundException {
-        //String pathClass = args[0];
-        //String methodName = args[1];
-        //String newMethodCode = args[2];
-        //String classCode = args[3];
-        //String otherCode = args[4];
-        System.out.println("----------------- Refactoring process has started .... -----------------");
-        Boolean result = codeModifier(pathClass, methodName, newMethodCode, classCode, otherCode);
-        //System.out.println(result);
-        System.out.println(" -------------------------- Refactoring process terminated .... ---------------------------");
-        //return result;
+        String pathClass = args[0];
+        String mainMethodName = args[1];
+        String mainMethodCode = args[2];
+        String otherMethodsCodeArg = args[3];
+        String classesCodeArg = args[4];
+        ArrayList<String> otherCode = new ArrayList<>();
+
+        ArrayList<String> otherMethodsCode = new ArrayList<>(Arrays.asList(otherMethodsCodeArg.split("/* */")));
+        otherMethodsCode.removeIf(str -> str.equals("* *"));
+
+        ArrayList<String> classesCode = new ArrayList<>(Arrays.asList(classesCodeArg.split("/* */")));
+        classesCode.removeIf(str -> str.equals("* *"));
+
+        codeModifier(pathClass, mainMethodName, mainMethodCode, otherMethodsCode, classesCode, otherCode);
     }
 }
 
--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,9 +1,10 @@
 import json
 from deepdiff import DeepDiff
+import subprocess
 
 def llm_generated_code_enhancer():
-    data_path = "/Users/jeancarlorspaul/Documents/Doc_Carl/Poly/LLM_Refactoring_Project/Refactoring_AST/src/llm_generated_code.json"
-    results_path = "/Users/jeancarlorspaul/Documents/Doc_Carl/Poly/LLM_Refactoring_Project/Refactoring_AST/src/llm_generated_code6.json"
+    data_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/llm_generated_code1.json"
+    results_path = "/src/run#9_processed.json"
     with open(data_path, 'r') as llm_generated_code_file:
         generated_code_data = json.load(llm_generated_code_file)
 
@@ -29,11 +30,26 @@ def results_comparator(result1_path, result2_path):
     print(diff)
 
 if __name__ == "__main__":
-    result1_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/results_summary.json"
-    result2_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/results_summary_run#1.json"
-    results_comparator(result1_path, result2_path)
+    #result1_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/results_summary.json"
+    #result2_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/results_summary_run#1.json"
+    #results_comparator(result1_path, result2_path)
+    #llm_generated_code_enhancer()
+
+    #print("public List<SrcOp> set(GrammarAST setAST, GrammarAST labelAST, boolean invert) {\n    MatchSet matchOp = createMatchSet(setAST, invert);\n    if (labelAST != null) {\n        handleLabelAST(labelAST, matchOp, setAST);\n    }\n    if (controller.needsImplicitLabel(setAST, matchOp)) {\n        defineImplicitLabel(setAST, matchOp);\n    }\n    AddToLabelList listLabelOp = getAddToListOpIfListLabelPresent(matchOp, labelAST);\n return list(matchOp, listLabelOp);\n}")
+    #print("private MatchSet createMatchSet(GrammarAST setAST, boolean invert) {\n    return invert ? new MatchNotSet(this, setAST) : new MatchSet(this, setAST);\n}")
+    #print("private void handleLabelAST(GrammarAST labelAST, MatchSet matchOp, GrammarAST setAST) {\n    String label = labelAST.getText();\n    Decl d = getTokenLabelDecl(label);\n    matchOp.labels.add(d);\n    getCurrentRuleFunction().addContextDecl(setAST.getAltLabel(), d);\n    if (labelAST.parent.getType() == ANTLRParser.PLUS_ASSIGN) {\n        TokenListDecl l = getTokenListLabelDecl(label);\n getCurrentRuleFunction().addContextDecl(setAST.getAltLabel(), l);\n    }\n}")
 
 
+    mainMethodCode = "mainMethodCode"
+    otherMethodsCode = ["otherMethodCode", "otherMethodCode", "otherMethodCode", "otherMethodCode"]
+    classesCode = ["classes", "classes", "classes", "classes"]
+    otherCode = ["otherCode", "otherCode", "otherCode", "otherCode"]
+
+    otherMethodsCode = "/* */".join(otherMethodsCode)
+    classesCode = "/* */".join(classesCode)
+    otherCode = "/* */".join(otherCode)
+
+    subprocess.call(['java', '-jar', "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/out/artifacts/Refactoring_LLM_Benchmark_main_jar/Refactoring_LLM_Benchmark.main.jar", "repository_path", "mainMethodName", mainMethodCode, otherMethodsCode, classesCode, otherCode])
 
 
 
