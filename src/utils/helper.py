9994a251430b72f72c242c77f907d5ab08f3d26e
jeanCarlors <jeancarlorspaul@yahoo.com>
2025-02-26 18:55:01 -0500

    Ground truth files added

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,6 +1,9 @@
 import json
 from deepdiff import DeepDiff
 import subprocess
+import os
+from git import Repo
+import shutil
 
 def llm_generated_code_enhancer():
     data_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/llm_generated_code1.json"
@@ -20,29 +23,78 @@ def llm_generated_code_enhancer():
         results_file.write(json_object)
 
 def data_collector():
-    other_results_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/results_summary_run#5.json"
-    enhanced_results_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/enhanced_results_summary_run#5.json"
-    with open(other_results_path, 'r') as result_collection_file:
-        other_results_collection = json.load(result_collection_file)
+    data_collection_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/Data_collection.json"
+    results_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/results"
+    junit4_repo = Repo("/Users/jeancarlorspaul/IdeaProjects/junit4/")
 
-    data_path = "/src/utils/Data_collection.json"
-    results_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/Data_collection_results.json"
-    with open(data_path, 'r') as Data_collection_file:
-        data_collection = json.load(Data_collection_file)
+    antlr4_repo = Repo("/Users/jeancarlorspaul/IdeaProjects/antlr4/")
+    antlr4_repo.git.checkout("5e05b71e8b1cd52cf0e77559786cc4c18dc85c37")
+    with open(data_collection_path, 'r') as data_collection_file:
+        data_collection = json.load(data_collection_file)
 
+    #results_path = "/Users/jeancarlorspaul/IdeaProjects/Refactoring_LLM_Benchmark/src/Data_collection_results.json"
+    #with open(data_path, 'r') as Data_collection_file:
+    #    data_collection = json.load(Data_collection_file)
+    project_list = ['antlr4', 'junit4']
+    local_repository = "/Users/jeancarlorspaul/IdeaProjects"
     for datum in data_collection:
-        print(datum["\ufeffID"])
-        for key, value in other_results_collection.items():
+        id = datum["\ufeffID"]
+        print(id)
+        for runs_path in os.listdir(results_path):
+            print(runs_path)
+            for project in project_list:
+                if project == 'antlr4':
+                    print(project)
+                    refactored_code_path = os.path.join(results_path, runs_path + "/" + project)
+                    for folder in os.listdir(refactored_code_path):
+                        if id in folder:
+                            print(folder)
+                            commitID_after = datum["commitID_after"]
+                            print(commitID_after)
+                            antlr4_repo.git.checkout(commitID_after)
+                            after_ground_truth_path = refactored_code_path + "/" + folder + "/" + 'after-ground-truth'
+                            print(after_ground_truth_path)
+                            os.makedirs(after_ground_truth_path, exist_ok=True)
+                            after_ground_truth_file = local_repository + "/" + project + "/" + datum["path_before"].replace("\\", "/")
+                            shutil.copy(after_ground_truth_file, after_ground_truth_path)
+
+                else:
+                    print(project)
+                    refactored_code_path = os.path.join(results_path, runs_path + "/" + project)
+                    for folder in os.listdir(refactored_code_path):
+                        if id in folder:
+                            print(folder)
+                            commitID_after = datum["commitID_after"]
+                            print(commitID_after)
+                            junit4_repo.git.checkout(commitID_after)
+                            after_ground_truth_path = refactored_code_path + "/" + folder + "/" + 'after-ground-truth'
+                            print(after_ground_truth_path)
+                            os.makedirs(after_ground_truth_path, exist_ok=True)
+                            after_ground_truth_file = local_repository + "/" + project + "/" + datum["path_before"].replace("\\", "/")
+                            shutil.copy(after_ground_truth_file, after_ground_truth_path)
+                print("-"*100)
+
+
+
+                #if id in folder:
+                #run_path = results_path +"/"+ runs_path
+                #antlr4_run_path = run_path + '/antlr4'
+                #for factored_code_path in os.listdir(antlr4_run_path):
+                 #   print(factored_code_path)
+        '''for key, value in other_results_collection.items():
             if datum["\ufeffID"] in key :
                 value["path_before"] = datum["path_before"].replace("\\", "/")
-                print(value.keys())
+                print(value.keys())'''
         print("="*150)
 
-    json_object = json.dumps(other_results_collection, indent=4)
 
-    with open(enhanced_results_path, 'w') as enhanced_results_file:
-        enhanced_results_file.write(json_object)
+
+    #json_object = json.dumps(other_results_collection, indent=4)
+
+    #with open(enhanced_results_path, 'w') as enhanced_results_file:
+     #   enhanced_results_file.write(json_object)
 
 
 if __name__ == "__main__":
     data_collector()
+
